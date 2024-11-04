28a1bf5eb866816ed6273a3b5098f4d23c3abcf9
sercuno <sercuno@users.noreply.github.com>
2024-11-04 12:03:50 +0100

    Initial commit from W3Schools

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function content_changer() {
+    document.getElementById("content").innerHTML = "Thank you for pressing the button"
+}
+
+// Will overwrite the page
+function write() {
+    document.write(1+5)
+}
+
+function alerting() {
+    window.alert("Hello!")
+}
+
+function logging() {
+    console.log(5+1)
+}
